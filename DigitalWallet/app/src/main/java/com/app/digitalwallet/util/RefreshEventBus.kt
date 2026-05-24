18509e425592c94ed147e1ea17723a6f3a95f56a
TheChhay <chhayprogram@gamil.com>
2026-05-24 15:26:43 +0700

    feature: notification

---
+++ DigitalWallet/app/src/main/java/com/app/digitalwallet/util/RefreshEventBus.kt
@@ -0,0 +1,22 @@
+package com.app.digitalwallet.util
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RefreshEventBus @Inject constructor() {
+    private val _refreshEvents = MutableSharedFlow<RefreshType>(extraBufferCapacity = 1)
+    val refreshEvents = _refreshEvents.asSharedFlow()
+
+    suspend fun emitRefresh(type: RefreshType) {
+        _refreshEvents.emit(type)
+    }
+
+    enum class RefreshType {
+        WALLET,
+        TRANSACTIONS,
+        ALL
+    }
+}
